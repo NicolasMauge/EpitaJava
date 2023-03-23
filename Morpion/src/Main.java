864ee0716f4784e854f07cfefce14fa12339f668
DESKTOP-ED1T2RE\lenic <grenats_geniale_0d@icloud.com>
2023-03-23 17:41:35 +0100

    Programmation du morpion

--- Morpion/src/Main.java
+++ Morpion/src/Main.java
@@ -1,5 +1,59 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Morpion morpion = new Morpion();
+        int[][] tableauDeJeu = new int[3][3];
+
+        tableauDeJeu = morpion.initBoard(tableauDeJeu);
+
+        morpion.drawBoard(tableauDeJeu);
+
+        int player = 1;
+
+        while(!morpion.isWinning(tableauDeJeu)) {
+            System.out.println(">>>>Player : "+player);
+            System.out.println("Entrez les coordonnées : ");
+
+            while(true) {
+                int x = askValue("Valeur x [1-3] : ");
+                int y = askValue("Valeur y [1-3] : ");
+
+                if (tableauDeJeu[y - 1][x - 1] == 0) {
+                    tableauDeJeu[y - 1][x - 1] = player;
+                    break;
+                } else {
+                    System.out.println("Cette position a déjà été jouée");
+                }
+            }
+
+            morpion.drawBoard(tableauDeJeu);
+
+            if (player == 1) {
+                player = 2;
+            } else {
+                player = 1;
+            }
+        }
+        System.out.println("Player " + player + " a perdu");
+    }
+
+    public static int askValue(String question) {
+        Scanner sc = new Scanner(System.in);
+        int value;
+
+        System.out.println(question);
+
+        while(true) {
+            value = sc.nextInt();
+
+            if((value<1) || (value>3)) {
+                System.out.println("Erreur");
+            } else {
+                break;
+            }
+        }
+
+        return value;
     }
 }
--- Morpion/src/Morpion.java
+++ Morpion/src/Morpion.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class Morpion {
+public class Morpion {
+
+    public static final int TABLE_SIZE = 3;
+
+    /**
+     * Affiche le plateau de jeu
+     * @param board état du plateau de jeu
+     */
+    public void drawBoard(int[][] board) {
+        String ligne = "";
+        System.out.println("|---|---|---|");
+
+        for(int j = 0; j< TABLE_SIZE; j++) {
+            ligne = "| ";
+            for (int i = 0; i < TABLE_SIZE; i++) {
+                ligne += String.valueOf(board[j][i]) + " | ";
+            }
+            System.out.println(ligne);
+            System.out.println("|---|---|---|");
+        }
+    }
+
+    /**
+     * initialisation du plateau de jeu avec des zéros
+     * @param board plateau de jeu non initialisé
+     * @return le plateau de jeu
+     */
+    public int[][] initBoard(int[][] board){
+        for(int i = 0; i< TABLE_SIZE; i++) {
+            for(int j = 0; j< TABLE_SIZE; j++) {
+                board[i][j] = 0;
+            }
+        }
+        return board;
+    }
+
+    /**
+     * est-ce qu'il y a un joueur qui a gagné ?
+     * @param board le plateau de jeu
+     * @return un boolean pour savoir si une combinaison est gagnante
+     */
+    public Boolean isWinning(int[][] board) {
+        // une ligne gagnante
+        for(int i = 0; i< TABLE_SIZE; i++) {
+            if((board[i][0] == board[i][1]) && (board[i][1] == board[i][2]) && (board[i][0] != 0)) {
+                return true;
+            }
+        }
+
+        // une colonne gagnante
+        for(int i = 0; i< TABLE_SIZE; i++) {
+            if((board[0][i] == board[1][i]) && (board[1][i] == board[2][i]) && (board[0][i] != 0)) {
+                return true;
+            }
+        }
+
+        // diagonale
+        if((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && (board[1][1] != 0)) {
+            return true;
+        }
+
+        if((board[2][0] == board[1][1]) && (board[1][1] == board[0][2]) && (board[1][1] != 0)) {
+            return true;
+        }
+        return false;
+    }
 }
