f9135b0ba5a622b5181de891286b5fca1c38ba85
DESKTOP-ED1T2RE\lenic <grenats_geniale_0d@icloud.com>
2023-03-23 12:09:35 +0100

    Ex 1, 2, 3

--- MonProjet/src/Main.java
+++ MonProjet/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a;
+        int b;
+        int temp;
+
+        a = 1;
+        b = 2;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + "," +  b);
+
+        System.out.println(euclidianDivision(a, b));
+
+        System.out.println(canBeDivided357(35));
+        System.out.println(canBeDivided357(105));
     }
+
+    public static Boolean canBeDivided357(int num) {
+        return isDivisor(num, 3) && isDivisor(num, 5) && isDivisor(num, 7);
+    }
+
+    public static Boolean isDivisor(int num, int divisor) {
+        return (num % divisor == 0);
+    }
+
+    public static int euclidianDivision(int num, int denom) {
+        return num / denom;
+    }
+
 }
