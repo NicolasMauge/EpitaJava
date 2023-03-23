e85d7ef43b9e3c14f4e1f69b50f3c4e13bffdd10
DESKTOP-ED1T2RE\lenic <grenats_geniale_0d@icloud.com>
2023-03-23 15:34:02 +0100

    javadoc

--- Ex8-9-10/src/Calculus.java
+++ Ex8-9-10/src/Calculus.java
@@ -1,8 +1,22 @@
 public class Calculus {
+    /**
+     * Cette fonction permet d'additionner deux entiers
+     *
+     * @param a : premier entier
+     * @param b : deuxième entier
+     * @return l'addition entre les deux entiers
+     */
     public int add2Int(int a, int b){
         return a + b;
     }
 
+    /**
+     * Cette fonction permet de faire une division entière
+     *
+     * @param a : numérateur, entier
+     * @param b : dénominateur, entier
+     * @return : la division entière du numérateur par le dénominateur
+     */
     public int euclideanDivision(int a, int b) {
         return a / b;
     }
