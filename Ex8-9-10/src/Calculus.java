35a0b0c5338c2c4447cebd747f916c641536b592
DESKTOP-ED1T2RE\lenic <grenats_geniale_0d@icloud.com>
2023-03-23 16:41:21 +0100

    Ajout factoriel

--- Ex8-9-10/src/Calculus.java
+++ Ex8-9-10/src/Calculus.java
@@ -18,6 +18,35 @@ public class Calculus {
      * @return : la division entière du numérateur par le dénominateur
      */
     public int euclideanDivision(int a, int b) {
+        if(b==0) {
+            System.out.println("Division par zéro");
+        }
+
         return a / b;
+    }
+
+    /** Calcul de factoriel par récursion
+     *
+     * @param n le nombre dont on veut avoir la factorielle
+     * @return la factorielle
+     */
+    public long factoriel(int n) {
+        if(n==0) {
+            return 1;
+        }
+        return n * factoriel(n-1);
+    }
+
+    /** Calcul de factoriel en itération
+     *
+     * @param n le nombre dont on veut avoir la factorielle
+     * @return la factorielle
+     */
+    public long factorielIter(int n) {
+        long result = 1;
+        for(int i=2; i <= n;i++) {
+            result *= i;
+        }
+        return result;
     }
 }
--- Ex8-9-10/src/Main.java
+++ Ex8-9-10/src/Main.java
@@ -2,13 +2,35 @@ public class Main {
     public static void main(String[] args) {
         Calculus calc = new Calculus();
 
+        int n = 60;
+
         int result = calc.add2Int(50, 30);
-        int result2 = calc.euclideanDivision(10,3);
+        int result2 = calc.euclideanDivision(10, 3);
 
         System.out.println("50 + 30 = " + result);
-
         System.out.println("10 // 3 = " + result2);
-    }
 
+        //System.out.println("10 // 0 = ");
+        //int result3 = calc.euclideanDivision(10, 0);
+
+        long startTime = System.nanoTime();
+        for(int i=0;i< 1000;i++) {
+            long value = calc.factoriel(n);
+            //System.out.println("Factoriel de " + n + " = " + calc.factoriel(n));
+        }
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println(duration);
+
+
+        startTime = System.nanoTime();
+        for(int i=0;i< 1000;i++) {
+            long value = calc.factorielIter(n);
+            //System.out.println("Factoriel de " + n + " (avec une boucle) = " + calc.factorielIter(n));
+        }
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.println(duration);
+    }
 
 }
