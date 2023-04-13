ddb7a52a3e35528e77948d5046d4ecb37d70887a
Nicolas Mauge <grenats_geniale_0d@icloud.com>
2023-04-13 11:01:32 +0200

    Changement de répertoire

--- Exercices/TP-Hibernate/src/main/java/org/example/application/ServiceFactory.java
+++
@@ -1,7 +0,0 @@
-package org.example.application;
-
-public class ServiceFactory {
-    public static VoitureServiceImpl getService(){
-        return new VoitureServiceImpl();
-    }
-}
--- Exercices/TP2_Jointure/src/main/java/org/example/dao/IClientDAO.java
+++
@@ -1,7 +0,0 @@
-package org.example.dao;
-
-import org.example.domaine.Client;
-
-public interface IClientDAO {
-    void createClient(Client client);
-}
