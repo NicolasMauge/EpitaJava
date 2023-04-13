dbd96e1ed39a1c440b9da789b5c31027a9803fc4
Nicolas Mauge <grenats_geniale_0d@icloud.com>
2023-04-13 11:23:36 +0200

    Changement de répertoire

---
+++ Spring/TP-SpringData/src/main/java/org/example/TPHBSPRING/TpHbspringApplication.java
@@ -0,0 +1,12 @@
+package org.example.TPHBSPRING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpHbspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpHbspringApplication.class, args);
+    }
+}
