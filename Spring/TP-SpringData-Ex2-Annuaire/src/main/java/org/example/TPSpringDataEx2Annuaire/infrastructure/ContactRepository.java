cfda13d3cccc38ef8601149ed9088b6c67a6155a
Nicolas Mauge <grenats_geniale_0d@icloud.com>
2023-04-13 11:25:18 +0200

    Ajout d'une possibilité de requête

---
+++ Spring/TP-SpringData-Ex2-Annuaire/src/main/java/org/example/TPSpringDataEx2Annuaire/infrastructure/ContactRepository.java
@@ -0,0 +1,18 @@
+package org.example.TPSpringDataEx2Annuaire.infrastructure;
+
+import org.example.TPSpringDataEx2Annuaire.domaine.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    List<Contact> findByFirstName(String firstName);
+
+    List<Contact> findByLastName(String lastName);
+
+    List<Contact> findByPhoneNumber(String phoneNumber);
+
+    List<Contact> findByLastNameAndFirstName(String lastName, String firstName);
+}
