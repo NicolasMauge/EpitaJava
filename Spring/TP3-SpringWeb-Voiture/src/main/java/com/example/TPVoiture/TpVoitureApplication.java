bb936a201b3e88f584b2076bfcd1f406fd873f25
Nicolas Mauge <grenats_geniale_0d@icloud.com>
2023-04-14 13:56:22 +0200

    Changement de répertoire

---
+++ Spring/TP3-SpringWeb-Voiture/src/main/java/com/example/TPVoiture/TpVoitureApplication.java
@@ -0,0 +1,13 @@
+package com.example.TPVoiture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpVoitureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpVoitureApplication.class, args);
+	}
+
+}
---
+++ Spring/TP3-SpringWeb-Voiture/src/main/java/com/example/TPVoiture/application/VoitureService.java
@@ -0,0 +1,7 @@
+package com.example.TPVoiture.application;
+
+public interface VoitureService {
+    String reparerVoiture();
+
+    Voiture findVoiture(int id);
+}
