f36820084f78e00c8782974557bfe40275f1e7a2
Nicolas Mauge <grenats_geniale_0d@icloud.com>
2023-04-14 14:00:06 +0200

    Changement de répertoire

---
+++ Spring/TP9-SpringData-Ex2-Annuaire/src/main/java/org/example/TPSpringDataEx2Annuaire/TpSpringDataEx2AnnuaireApplication.java
@@ -0,0 +1,13 @@
+package org.example.TPSpringDataEx2Annuaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringDataEx2AnnuaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringDataEx2AnnuaireApplication.class, args);
+	}
+
+}
---
+++ Spring/TP9-SpringData-Ex2-Annuaire/src/test/java/org/example/TPSpringDataEx2Annuaire/TpSpringDataEx2AnnuaireApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.TPSpringDataEx2Annuaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringDataEx2AnnuaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
