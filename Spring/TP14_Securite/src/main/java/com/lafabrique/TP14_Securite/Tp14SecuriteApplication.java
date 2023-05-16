2647bcebc7f98c1ce3542149cb24ff3e0645fe40
Nicolas Mauge <36577679+NicolasMauge@users.noreply.github.com>
2023-05-16 15:01:41 +0200

    main

---
+++ Spring/TP14_Securite/src/main/java/com/lafabrique/TP14_Securite/Tp14SecuriteApplication.java
@@ -0,0 +1,13 @@
+package com.lafabrique.TP14_Securite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp14SecuriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp14SecuriteApplication.class, args);
+	}
+
+}
