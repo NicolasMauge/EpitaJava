7f84bfccf510accbd4efee28b9c0890858799c82
Nicolas Mauge <36577679+NicolasMauge@users.noreply.github.com>
2023-05-22 16:14:06 +0200

    création du generateur de token

---
+++ Spring/TP16-generateToken/src/main/java/TP16generateToken/configuration/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package TP16generateToken.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().denyAll()
+                .and()
+                .addFilter(new JWTAuthenticationManager(
+                        authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)
+                        )))
+                .csrf().disable()
+                .formLogin().disable();
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
---
+++ Spring/TP16-generateToken/src/main/java/TP16generateToken/dto/RoleDto.java
@@ -0,0 +1,20 @@
+package TP16generateToken.dto;
+
+public class RoleDto {
+    private String nom;
+
+    public RoleDto(String nom) {
+        this.nom = nom;
+    }
+
+    public RoleDto() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
