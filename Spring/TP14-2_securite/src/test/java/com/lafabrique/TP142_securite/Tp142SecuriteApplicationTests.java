c510eab31e163b09be60237ff16b3f999e427203
Nicolas Mauge <36577679+NicolasMauge@users.noreply.github.com>
2023-05-22 14:25:14 +0200

    mdoifications

---
+++ Spring/TP14-2_securite/src/test/java/com/lafabrique/TP142_securite/Tp142SecuriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lafabrique.TP142_securite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp142SecuriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
