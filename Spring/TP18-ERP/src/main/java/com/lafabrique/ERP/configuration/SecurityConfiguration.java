eb3d0fc0bcbc512c3af0bcb688a20b643b668eb1
Nicolas Mauge <36577679+NicolasMauge@users.noreply.github.com>
2023-05-31 11:08:06 +0200

    Ajout du verrouillage par token

---
+++ Spring/TP18-ERP/src/main/java/com/lafabrique/ERP/configuration/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.lafabrique.ERP.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    /*@Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
+    }*/
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/v1/").hasRole("ADMIN");
+        // ajouter d'autres autorisations sur les autres pages
+        // http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/v1/hello/admin/getAdminHello").hasRole("ADMIN");
+        http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.csrf().disable();
+        http.formLogin().disable();
+        return http.build();
+    }
+}
